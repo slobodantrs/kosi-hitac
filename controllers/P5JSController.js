5f9cca2cda4678697c0e48b79a35a30fa7b36de6
slobodantrs <slobodantrs@gmail.com>
2025-04-12 20:07:56 +0200

    popravka menija

--- controllers/P5JSController.js
+++ controllers/P5JSController.js
@@ -1,20 +1,20 @@
 var methods={
 
 Uvod_u_p5js: function(req, res) {
-	alert("Uvod_u_p5js");
 	
+	console.log("Uvod_u_p5js");
     res.render('pages/upotrebaP5js/opis_p5js');
 
 },
 Animacija_kruznog_kretanja: function(req, res) {
-	alert("Animacija_kruznog_kretanja");
+	console.log("Animacija_kruznog_kretanja");
 	
 	
     res.render('pages/upotrebaP5js/kruzno_kretanje_animacija_p5js');
 
 },
 Klizanje_tela_niz_strmu_ravan: function(req, res) {
-	
+	console.log("Klizanje_tela_niz_strmu_ravan");
     res.render('pages/upotrebaP5js/klizanje_tela_niz_strmu_ravan_animacija');
 
 },
