d46b54271b9c634af892d9f0a9c626540e8e354c
slobodantrs <slobodantrs@gmail.com>
2025-04-18 18:08:54 +0200

    Funkcionalnost jezika

--- app.js
+++ app.js
@@ -1,103 +1,98 @@
+// 1) imports
+const express      = require('express');
+const path         = require('path');
+const cookieParser = require('cookie-parser');
+const proxy        = require('express-http-proxy');
+const i18n         = require('i18n');
+const routes       = require('./routes');
+const seo          = require('express-seo');
 
-
-
-const path=require('path');
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const router = express.Router();
+// 2) init
+const app  = express();
 const PORT = process.env.PORT || 5000;
-const http=require('http');
-const proxy = require("express-http-proxy");
 
-const routes = require('./routes');
-//const controllers = require('./controllers');
-
-//server.listen(PORT);
-var app=express(); 
- app.use(express.static(path.join(__dirname, 'public')));
- app.use(express.static(path.join(__dirname, 'testHTML5')));
- app.use(express.static(path.join(__dirname, 'node_modules')));
- app.use('/', routes);
- // app.use('/', controllers);
-  app.set('views', [path.join(__dirname, 'views'),path.join(__dirname, 'views/partials')]);
-  
-  app.use(express.static(__dirname)); // serve static files from cwd
-
-function getIpFromReq (req) { // get the client's IP address
-    var bareIP = ":" + ((req.connection.socket && req.connection.socket.remoteAddress)
-        || req.headers["x-forwarded-for"] || req.connection.remoteAddress || "");
-    return (bareIP.match(/:([^:]+)$/) || [])[1] || "127.0.0.1";
-}
-
-// proxying requests from /analytics to www.google-analytics.com.
-app.use("/analytics", proxy("www.google-analytics.com", {
-    proxyReqPathResolver: function (req) {
-        return req.url + (req.url.indexOf("?") === -1 ? "?" : "&")
-            + "uip=" + encodeURIComponent(getIpFromReq(req));
-    }
-}));
-  
-  
-  
-
-
-
-
-  
- const options = {
-  
-  headers: {
-    'Content-Type': 'text/plain;charset=UTF-8',
-  }
-};
-router.get('/robots.txt', (req, res) => (
-  res.status(200).sendFile('robots.txt', options)
-));
-
- 
-   app.set("view options", {layout: false});  
-  // app.engine('xhtml', require('ejs').renderFile); 
-   
-  // app.set('view engine', 'xhtml');
-  
-// Set the view engine to ejs
-app.set('view engine', 'ejs');
-
-
-  
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-
-var seo = require('express-seo')(app);
- 
-// For internatanalization, set the supported languages
-seo.setConfig({
-    langs: ["en", "fr","sr-rs"]
+i18n.configure({
+  locales: ['sr','en'],
+  directory: path.join(__dirname,'locales'),
+  defaultLocale: 'sr',
+  queryParameter: 'lang',
+  cookie: 'lang',
+  autoReload: true,
+  syncFiles: true
 });
- 
-// Set the default tags
-seo.setDefaults({
-    html: "<a href='https://www.facebook.com/izprogramiranja/?modal=admin_todo_tour'>Sledi me na fejsbuku</a>", // Special property to insert html in the body (interesting to insert links)
-    title: "Iz programiranja", // Page title
-    // All the other properties will be inserted as a meta property
-    description: {
-        en: "Check out my awesome website",
-        fr: "Decouvez mon incroyable site"
-    },
-    image: "https://assets-cdn.github.com/images/modules/dashboard/bootcamp/octocat_setup.png"
-});
- 
-// Create an seo route
-seo.add("/contact", function(req, opts, next) {
-    /*
-    req: Express request
-    opts: Object {
-        service: String ("facebook" || "twitter" || "search-engine")
-        lang: String (Detected language)
-    }
-    */
-    next({
-        description: "Amazing contact page"
+
+app.use(cookieParser());
+app.use(i18n.init);
+
+//  –––––––––––––––––––––––––––––––––––––––––––––––––
+// Middleware: promena jezika preko ?lang=xx i kolačići
+app.use((req, res, next) => {
+  // 1) Ako u URL‑u stoji ?lang=sr ili ?lang=en …
+  if (req.query.lang) {
+    // 2) … sačuvaj ga u kolačić
+    res.cookie('lang', req.query.lang, {
+      maxAge: 30 * 24 * 60 * 60 * 1000, // npr. 30 dana
+      httpOnly: true
     });
+    // 3) i odmah namesti locale na req
+    req.setLocale(req.query.lang);
+  }
+  next();
+});
+//  –––––––––––––––––––––––––––––––––––––––––––––––––
+app.use((req, res, next) => {
+  res.locals.__     = res.__;
+  res.locals.locale = req.getLocale();
+  next();
 });
 
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'testHTML5')));
+app.use(express.static(path.join(__dirname,'node_modules')));
+app.use(express.static(__dirname));
+
+// 3) robots.txt
+const router = express.Router();
+router.get('/robots.txt', (req, res) => {
+  res.type('text/plain; charset=UTF-8');
+  res.sendFile(path.join(__dirname, 'robots.txt'));
+});
+app.use(router);
+
+// 4) views
+app.set('views', [
+  path.join(__dirname,'views'),
+  path.join(__dirname,'views/partials')
+]);
+app.set('view engine', 'ejs');
+app.use('/', routes);
+
+// 5) analytics proxy
+function getIpFromReq(req) {
+  let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  return ip.split(',')[0].trim();
+}
+app.use("/analytics", proxy("www.google-analytics.com", {
+  proxyReqPathResolver: req =>
+    req.url + (req.url.includes("?") ? "&" : "?") +
+    "uip=" + encodeURIComponent(getIpFromReq(req))
+}));
+
+// 6) SEO
+seo(app);
+seo.setConfig({ langs: ["en","fr","sr-rs"] });
+seo.setDefaults({
+  html: "<a href='https://www.facebook.com/svetprogramiranja'>Prati nas na Facebook‑u</a>",
+  title: "Kosi Hitac – Interaktivne simulacije iz fizike",
+  description: {
+    en: "Explore interactive physics simulations built with EJS and p5.js.",
+    sr: "Interaktivne simulacije iz fizike kreirane pomoću EJS i p5.js."
+  },
+  image: "https://kosi-hitac.onrender.com/images/preview-kosi-hitac.png"
+});
+
+// 7) start
+app.listen(PORT, () => {
+  console.log(`Listening on ${ PORT }`);
+});
