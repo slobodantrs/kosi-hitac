c1882584d99efa0f4498baaae5d4d4634e2360b8
slobodantrs <slobodantrs@gmail.com>
2025-04-22 17:07:55 +0200

    preimenovanje putanja na engleski

--- controllers/VertikalanHitacController.js
+++ controllers/VertikalanHitacController.js
@@ -29,7 +29,7 @@ var methods = {
 	console.log('path 1>'+res.path1);
 	console.log('path 2>'+res.path2);
 	
-console.log("Render attempt:", 'pages/ejss_model_VertikalanHitac/vertikalanHitac');
+console.log("Render attempt:", 'pages/ejss_model_KosHitac/vertikalanHitac');
 
    
 	
