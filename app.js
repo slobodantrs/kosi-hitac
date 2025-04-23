5bc3dc11ceccded4789a071a566a3a3630a4a67c
slobodantrs <slobodantrs@gmail.com>
2025-04-23 10:30:55 +0200

    preimenovanje putanja na engleski

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ console.log('EN catalog:', i18n.getCatalog('en'));
 
 // после i18n.init
 console.log('i18n locales:', i18n.getLocales());
-console.log('i18n directory:', i18n.options.directory);
+console.log('i18n directory:', i18n.settings.directory);
 
 try {
-  const files = fs.readdirSync(i18n.options.directory);
+  const files = fs.readdirSync(i18n.settings.directory);
   console.log('Files in locales dir:', files);
 } catch (err) {
   console.error('Error reading locales directory:', err);
