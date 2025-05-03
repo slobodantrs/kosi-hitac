218d212d9220fd2ab32a8606fbbb656f572635d5
slobodantrs <slobodantrs@gmail.com>
2025-05-03 11:28:54 +0200

    prevod na engleski 3

--- controllers/VertikalanHitacController.js
+++ controllers/VertikalanHitacController.js
@@ -65,20 +65,20 @@ VertikalanHitac_EN: function(req, res) {
   res.id = id;
   res.path1 = './verticalThrow_Contents';
   res.path2 = (id == 2)
-    ? './verticalThrow_Intro_2'
-    : './verticalThrow_Intro_1';
+    ? './VerticalThrow_Intro_2'
+    : './VerticalThrow_Intro_1';
   console.log(`Vertical throw controller EN ${id}, ${res.path1}, ${res.path2}`);
-  res.render('pages-en/ejss_model_VerticalThrow/verticalThrow', res);
+  res.render('pages-en/ejss_model_VerticalThrow/VerticalThrow', res);
 },
 
 // Contents page
 VertikalanHitacContents_EN: function(req, res) {
-  res.render('pages-en/ejss_model_VerticalThrow/verticalThrow_Contents');
+  res.render('pages-en/ejss_model_VerticalThrow/VerticalThrow_Contents');
 },
 
 // Simulation page
 VertikalanHitac_Simulation_EN: function(req, res) {
-	res.path1 = './vertikalanHitac_Contents';
+	res.path1 = './VertikalanHitac_Contents';
    
    
 	console.log('path 1>'+res.path1);
