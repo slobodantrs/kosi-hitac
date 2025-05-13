74648377425125019cbb7ea7f5e5ac5e8bb90c59
slobodantrs <slobodantrs@gmail.com>
2025-05-13 21:41:17 +0200

    Sunce Zemlja mesec animacija

--- public/js/p5js/ZemljaSunceMesec/sunce_zemlja_mesec_animacija.js
+++ public/js/p5js/ZemljaSunceMesec/sunce_zemlja_mesec_animacija.js
@@ -59,7 +59,7 @@ function setup() {
  function preload(){
     sunIm=loadImage(prefix+"Textures/sun1.jpg");
   earthIm=loadImage(prefix+"Textures/earth1.jpg", img => {
-    img.resize(512, 512);
+    img.resize(512, 0);
   });
   moonIm=loadImage(prefix+"Textures/moon.jpg");
   spaceIm=loadImage(prefix+"Textures/svemir1.jpg");
