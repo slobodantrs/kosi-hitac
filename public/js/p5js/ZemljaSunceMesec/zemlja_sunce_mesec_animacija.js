d99198a26810bbab83137ca11baa4dee263402f5
slobodantrs <slobodantrs@gmail.com>
2025-05-16 12:38:11 +0200

    3d animacija rotacija zemlje i meseca

--- public/js/p5js/ZemljaSunceMesec/zemlja_sunce_mesec_animacija.js
+++ public/js/p5js/ZemljaSunceMesec/zemlja_sunce_mesec_animacija.js
@@ -39,7 +39,7 @@ function setup() {
   rSun=widthS/20;
    rEarth= rSun/4;
    rMoon= rEarth/3;
-  sun=new Planeta( rSun,0,null,sunIm,0,0,color(255, 204, 0));
+  sun=new Planeta( rSun,0,null,sunIm,0,0,color(255));
   earth=new Planeta(rEarth,widthS/3,sun,earthIm,wEarth,wEarth1);
   moon=new Planeta(rMoon,rEarth+rMoon+25,earth,moonIm,wMoon,0);
   canvasDrawing.parent(container);
@@ -182,7 +182,7 @@ class Planeta{
       if (this.emission) {
     
         fill(this.emission);
-		emissiveMaterial(255, 204, 0);   // Sunce samo svetli  
+	//	emissiveMaterial(255, 204, 0);   // Sunce samo svetli  
         scale(100);
       
         pointLight(this.emission, drag.x+dLight, drag.y, z);
@@ -213,8 +213,8 @@ class Planeta{
         else{
            ambientMaterial(255);
         }
-        specularMaterial(0, 100, 255);   // plavi sjaj vode
-        shininess(30);
+    //    specularMaterial(0, 100, 255);   // plavi sjaj vode
+    //    shininess(30);
         sphere(this.radius);
       }
       
