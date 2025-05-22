6941d108b5debbe37afd99e1c425eb91c67be3b0
slobodantrs <slobodantrs@gmail.com>
2025-05-22 08:32:06 +0200

    Dodavanje okruženja za javu

--- controllers/OSPController.js
+++ controllers/OSPController.js
@@ -1,6 +1,7 @@
 // controllers/OSPController.js
 methods = {
   Uvod_U_Osp(req, res) {
+	  console.log('Render attempt:', 'pages/OSP/uvod_u_osp');
     res.render('pages/OSP/uvod_u_osp', {
       locale: req.getLocale(),
       title:    res.__('osp.title'),
@@ -9,6 +10,7 @@ methods = {
   },
 
   Uvod_U_Osp_EN(req, res) {
+	  console.log('Render attempt:', 'pages-en/OSP/introduction_to_osp');
     res.render('pages-en/OSP/introduction_to_osp', {
       locale: req.getLocale(),
       title:    res.__('osp.title'),
@@ -17,6 +19,7 @@ methods = {
   },
   
   IDE_Okruzenje_Osp(req, res) {
+	  console.log('Render attempt:', 'pages/OSP/ide_okruzenje_osp');
   res.render('pages/OSP/ide_okruzenje_osp', {
     locale: req.getLocale(),
     title: res.__('osp.ide.title'),
@@ -25,6 +28,7 @@ methods = {
 },
 
 IDE_Environment_Osp_EN(req, res) {
+	console.log('Render attempt:', 'pages-en/OSP/ide_environement_osp');
   res.render('pages-en/OSP/ide_environement_osp', {
     locale: req.getLocale(),
     title: res.__('osp.ide.title'),
