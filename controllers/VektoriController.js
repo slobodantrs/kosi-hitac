61f603263aa02fabacf1a0a8a5beb39e0aabb2c7
slobodantrs <slobodantrs@gmail.com>
2025-06-25 18:06:30 +0200

    dodavanje meta za vektore

--- controllers/VektoriController.js
+++ controllers/VektoriController.js
@@ -3,13 +3,17 @@ module.exports = {
   // Serbian version
   VektoriAnimacija: (req, res) => {
     res.render('pages/upotrebaP5js/vektori_animacija', {
-      locale: req.getLocale()
+      locale: req.getLocale(),
+	  pageDescription: res.__('meta.vektori_animacija.description'),
+      pageKeywords:    res.__('meta.vektori_animacija.keywords')
     });
   },
   // English version
   VektoriAnimacija_EN: (req, res) => {
     res.render('pages-en/usingP5js/vector_animation', {
-      locale: req.getLocale()
+      locale: req.getLocale(),
+	  pageDescription: res.__('meta.vector_animation.description'),
+      pageKeywords:    res.__('meta.vector_animation.keywords')
     });
   }
 };
